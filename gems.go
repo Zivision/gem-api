7f0b8aa031569dacc34a6170b371f892a995d338
Zivision <zcastro703@gmail.com>
2024-03-20 17:12:05 -0500

    Added POST endpoint and middleware

--- gems.go
+++ gems.go
@@ -12,7 +12,7 @@ import (
 type Gemstone struct {
 	ID 		 int 	   `json:"id"`
 	Name	 string    `json:"name"`
-	Hardness int       `json:"mohs hardness rating"`
+	Hardness int       `json:"hardness"`
 	Color 	 string    `json:"color"`
 }
 
@@ -20,14 +20,6 @@ var (
 	gems = map[int]*Gemstone{}
 	idCount = 1
 	lock = sync.Mutex{}
-
-	// Test data will delete later
-	diamond = &Gemstone{
-		ID: idCount,
-		Name: "Diamond",
-		Hardness: 10,
-		Color: "Clear",
-	}
 );
 
 /*
@@ -47,4 +39,23 @@ func listGem(c echo.Context) error {
 	defer lock.Unlock();
 	id, _ := strconv.Atoi(c.Param("id"));
 	return c.JSON(http.StatusOK, gems[id]);
+}
+
+/*
+ * Create gem POST
+ */
+
+func createGem(c echo.Context) error {
+	lock.Lock();
+	defer lock.Unlock();
+
+	g := &Gemstone{
+		ID: idCount,
+	}
+
+	if err := c.Bind(g); err != nil { return err }
+	gems[g.ID] = g;
+	idCount++;
+
+	return c.JSON(http.StatusOK, map[string]string{ "Message": "Gemstone added"})
 }
--- main.go
+++ main.go
@@ -1,28 +1,25 @@
 package main
 
 import (
-	"net/http"
-
 	// Echo
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 )
 
 func main() {
 	// Setup echo app
 	e := echo.New();
 
-	// Test end point ping
-	e.GET("/ping", func (c echo.Context) error {
-		return c.HTML(http.StatusOK, "<p>Pong!</p>");
-	});
-
-	// Add test data to map
-	gems[diamond.ID] = diamond;
+	// Middleware
+	e.Use(middleware.CORS())
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
 
 	// Gem api endpoints
 	g := e.Group("/api/gems");
 	g.GET("/", listGems);
 	g.GET("/:id", listGem);
+	g.POST("/", createGem);
 
 	// Start Server
 	e.Logger.Fatal(e.Start(":8080"));
